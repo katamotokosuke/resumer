dbe5b92b787b5313370721460566711567b2eab0
katamotokosuke <katamotokosuke0605@gmail.com>
2021-06-01 21:42:03 +0900

    雛形作成

---
+++ src/main/kotlin/com/kkatamot/resumer/ResumerApplication.kt
@@ -0,0 +1,11 @@
+package com.kkatamot.resumer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ResumerApplication
+
+fun main(args: Array<String>) {
+	runApplication<ResumerApplication>(*args)
+}
